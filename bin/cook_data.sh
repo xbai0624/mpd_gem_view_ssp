720b63b2580f23f584be812408f19626363084c1
Xinzhan Bai <xinzhan.uva@gmail.com>
2023-05-20 01:58:49 -0400

    add cook data script

---
+++ bin/cook_data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -i
+
+#./bin/replay --pedestal database/gem_ped_55.dat --common_mode database/CommonModeRange_55.txt --output_root_file ./Rootfiles/result_6.root /home/xinzhan/evio_data/fermilab_test/fermilab_beamtest_6.evio.0
+
+./bin/replay --pedestal database/gem_ped_55.dat --common_mode database/CommonModeRange_55.txt /home/xinzhan/evio_data/fermilab_test/fermilab_beamtest_6.evio.0
+
