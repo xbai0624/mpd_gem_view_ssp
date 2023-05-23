b6f28fc13387fb59849cf13cf2192c57a17b2930
Xinzhan Bai <xinzhan.uva@gmail.com>
2023-05-23 14:00:20 -0400

    .

--- bin/cook_data.sh
+++ bin/cook_data.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -i
 
+# if you want to save ROOT files to a specific directory:
 #./bin/replay --pedestal database/gem_ped_55.dat --common_mode database/CommonModeRange_55.txt --output_root_path /home/xinzhan/xb /home/xinzhan/evio_data/fermilab_test/fermilab_beamtest_6.evio.0
 
+# if you want to save ROOT files to the default directory: "./Rootfiles/xxxxx.root"
 ./bin/replay --pedestal database/gem_ped_55.dat --common_mode database/CommonModeRange_55.txt /home/xinzhan/evio_data/fermilab_test/fermilab_beamtest_8.evio.0
 
